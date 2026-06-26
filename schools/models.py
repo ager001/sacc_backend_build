83fdea2baa76075174479458f0a40cf0b10c82fa
ager001 <152964009+ager001@users.noreply.github.com>
2026-06-26 15:44:48 +0300

    Refactor user manager class to SchoolManager and update comments for clarity

--- schools/models.py
+++ schools/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-
-class CustomSchoolAuth(BaseUserManager):
+# A manager talks to the database, they answer CRUD operations questions
+# SchoolManager inherits everything from BaseUserManager
+# Now SchoolManager already knows many authentication-related operations.
+# We only need to customize what we want
+class SchoolManager(BaseUserManager):
+    # Below we have defined how a normal user should be created 
+    # Required pieces of information is; email(instead of username), password=None simply stores password safely
+    # args=Accept extra unnamed arguments kwargs=Accept extra named arguments.
     def create_user(self, email, password=None, *args, **kwargs):
+        #if no email provided, raise ValueError, if yes execute next line
         if not email:
             raise ValueError("Kindly input your email for your email is required")
