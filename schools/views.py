f930eb2728aee207629f905c0cdfd3dd993b67bc
ager001 <152964009+ager001@users.noreply.github.com>
2026-06-26 19:58:41 +0300

    Implement SchoolRegistrationAPIView and ListRegisteredSchoolsAPIView with appropriate permissions and serializers

--- schools/views.py
+++ schools/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from permissions import IsSuperUser
+from rest_framework import permissions,status
+from .serializers import (
+    SchoolRegistrationSerializer,
+    SchoolListSerializer,
+)
+from .models import School
 
-# Create your views here.
+# creation of school registration API
+class SchoolRegistrationAPIView(APIView):
+    # who is allowed to make the request(any=everyone)
+    permission_classes = [permissions.AllowAny]
+    # http method post means create
+    def post(self, request):
+        # validation of the request using serializer
+        serializer = SchoolRegistrationSerializer(
+                data=request.data
+            )
+        # after finding the validation is valid
+        serializer.is_valid()
+        # save 
+        serializer.save()
+        
+        # finally send back the data of the newly created school and the status code
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+        
+
+class ListRegisteredSchoolsAPIView(APIView):
+    # who is allowed to make the request
+    # only SuperUser is allowed to make request
+    # we have created custom permissions and imported it
+    permission_classes = [IsSuperUser]
+    # Http method
+    def get (self, request):
+        # here the model talks to the database.
+        schools = School.objects.all()
+        #Validation of the request using serializer
+        serializer = SchoolListSerializer(
+            schools, many=True
+        )
+        # after finding the validation is valid
+        serializer.is_valid()
+        # save 
+        serializer.save()
+        
+        # finally send back the data of the newly created school and the status code
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+    
