a3761fe40f68aad7b3c6f19e16fcff24195e5294
ager001 <152964009+ager001@users.noreply.github.com>
2026-06-28 21:18:53 +0300

    Add validation logic to LoginSerializer for user authentication

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -12,3 +12,30 @@ class LoginSerializer(serializers.Serializer):
         write_only=True,
         trim_whitespace=False,
     )
+    
+    # After field validation the code below runs
+    def validate(self, attrs):
+        # attrs is like a backpack containing validated details from serializers
+        # below we are taking out the email and password from the backpack
+        email = attrs.get("email")
+        password = attrs.get("password")
+        # below runs a DRF function that simply means "only verify identity"
+        # expects three things; request, email, password
+        school = authenticate(
+            request=self.context.get("request"),
+            email=email,
+            password=password
+        )
+        # below is a code for safety measures if authentication fails 
+        # we are checking against "None" not "False"
+        # because the inbuilt authenticate() should always return either User object or None
+        if school is None:
+            raise serializers.ValidationError(
+                "Invalid email or password."
+            )
+        # another important line here, 
+        # instead of making our views.py authenticate again, this line places our authenticated object into our back pac
+        # so in simple terms only our views.py can import and retrieve it
+        attrs["school"] = school
+
+        return attrs
